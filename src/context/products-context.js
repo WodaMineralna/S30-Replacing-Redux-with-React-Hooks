34dbce2edaded66805b4662a219c974a94b262ea
WodaMineralna <raknieborak65@gmail.com>
2025-07-02 19:28:56 +0200

    Minor code cleanup

--- src/context/products-context.js
+++ src/context/products-context.js
@@ -1,5 +1,5 @@
 import { createContext, useState } from "react";
-import { toggleFav } from "../store/actions/products";
+// import { toggleFav } from "../store/actions/products";
 
 export const ProductsContext = createContext({
   products: [],
